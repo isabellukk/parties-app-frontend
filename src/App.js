2a350e573e1ced29135eaace0f60c183c9baa6b1
Jess LaFrank <jesslafrank@gmail.com>
2021-09-30 15:33:21 -0700

    Removed cached data and placed node_modules in gitignore

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [parties, setParties] = useState([])
+
+  const getParties = async () => {
+    try {
+      const parties = await fetch("http://localhost:9000/parties")
+      const parsedParties = await parties.json()
+      setParties(parsedParties)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(()=>{
+    getParties()
+  }, [])
+
   return (
     <div>
-      :D
+      <h1>Parties App</h1>
     </div>
   );
 }
